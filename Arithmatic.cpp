c7eb3bce812a8e7887e3a341945cd0c6e01edfdd
rahul-rathava <rathavarahul870@gmail.com>
2024-03-05 04:44:41 +0530

    unstached file

---
+++ Arithmatic.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+int add()
+{
+    int a = 2, b = 4, c;
+    c = a + b;
+    cout << "Addition =" << c << endl;
+    return c;
+}
+
+int sub()
+{
+    int a = 2, b = 4, c;
+    c = b - a;
+    cout << "Subtraction =" << c << endl;
+    return c;
+}
+
+int mul()
+{
+    int a = 2, b = 4, c;
+    c = b * a;
+    cout << "Multiplication =" << c << endl;
+    return c;
+}
+
+int main()
+{
+
+    int x = add();
+    int y = sub();
+    int z = mul();
+    cout << x << endl;
+    return 0;
+}
